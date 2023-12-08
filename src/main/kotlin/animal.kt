c4125bbd81c7ad6028cac54021bb03776817e9e1
yo <harabiyousra12@gmail.com>
2023-12-08 18:44:43 +0100

    hii

--- src/main/kotlin/animal.kt
+++ src/main/kotlin/animal.kt
@@ -1,7 +1,10 @@
-class Animal(var hieght : Float,
-             var aga : Int,
-             var weight : Float,
-             var state :Boolean
+class Animal(
+    var hieght: Float,
+    var aga: Int,
+    var weight: Float,
+    var isAlive:Boolean
 ){
+fun Eat(food: String?):{
 
+}
 }
